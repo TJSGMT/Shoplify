6b1218fa24c8b871ebbffed62b7a5633c8bc87eb
tjs <tejas.g@simformsolutions.com>
2022-07-23 14:16:14 +0530

    Add sales count to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_21_125902) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_23_084113) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "sales_count", default: 0, null: false
   end
 
 end
